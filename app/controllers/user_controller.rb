6b074f5a12b57ffcb79a2476ccbe8b19b0812638
benaitcheson <benaitcheson@hotmail.com>
2022-05-28 23:01:43 +1000

    lots of user controller functions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,5 +6,30 @@ class UserController < ApplicationController
     @id = params[:id]
   end
 
+  def show
+  end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
